1464cd69f7a9f16655eeec7581b5ad72e186d4f0
Istvan Fehervari <gooksl@gmail.com>
2017-05-30 17:39:12 +0200

    Added a background fetch-based solution

--- IOSDataSharing/HSReplayClient/AppDelegate.swift
+++ IOSDataSharing/HSReplayClient/AppDelegate.swift
@@ -10,37 +10,55 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+	
 	var window: UIWindow?
-
-
+	
+	let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+	var dataTask: URLSessionDataTask?
+	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
 		return true
 	}
-
+	
+	// Support for background fetch
+	func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+		NSLog("Fetching data...")
+		let url = URL(string: "http://localhost:8080")
+		
+		dataTask = self.defaultSession.dataTask(with: url!, completionHandler: { (data, response, error) in
+			if let data = data {
+				let datastr = String(data: data, encoding: String.Encoding.utf8)
+				print(datastr!)
+			}
+			completionHandler(.newData)
+		})
+		dataTask?.resume()
+	}
+	
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 	}
-
+	
 	func applicationDidEnterBackground(_ application: UIApplication) {
 		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 	}
-
+	
 	func applicationWillEnterForeground(_ application: UIApplication) {
 		// Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 	}
-
+	
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 	}
-
+	
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
-
-
+	
+	
 }
 
--- IOSDataSharing/HSReplayHost/AppDelegate.swift
+++ IOSDataSharing/HSReplayHost/AppDelegate.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import Swifter
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
-
+	let server = HttpServer()
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
@@ -22,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillResignActive(_ application: UIApplication) {
 		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 		// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+		server.stop()
 	}
 
 	func applicationDidEnterBackground(_ application: UIApplication) {
@@ -35,6 +37,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+		server["/hello"] = { .ok(.html("You asked for \($0)"))  }
+		do {
+			try server.start()
+			print("Server started")
+		} catch {
+			print("Failed to start server")
+		}
 	}
 
 	func applicationWillTerminate(_ application: UIApplication) {
